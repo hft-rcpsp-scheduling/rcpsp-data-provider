4dba64d4a7f81a1b5e3bb10217c347cf4a34ac4a
felixsteinke <steinke.felix@yahoo.de>
2022-10-21 00:41:37 +0200

    fix test for changed body datatype

--- src/test/java/com/hft/provider/controller/EvaluationControllerTest.java
+++ src/test/java/com/hft/provider/controller/EvaluationControllerTest.java
@@ -1,7 +1,8 @@
 package com.hft.provider.controller;
 
 import com.hft.provider.Application;
-import com.hft.provider.controller.model.Feedback;
+import com.hft.provider.controller.model.Project;
+import com.hft.provider.file.ProjectReader;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,9 +15,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import utility.JsonFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,8 +33,8 @@ class EvaluationControllerTest {
 
     @Test
     void validate() throws Exception {
-        List<Feedback> requestObj = new ArrayList<>();
-        String requestJson = JsonFactory.convertToJson(requestObj);
+        Project project = new ProjectReader().parseProject("projects/j30/j301_1.sm");
+        String requestJson = JsonFactory.convertToJson(project);
         MvcResult result = mockMvc.perform(
                         post("/api/eval")
                                 .contentType(MediaType.APPLICATION_JSON)
