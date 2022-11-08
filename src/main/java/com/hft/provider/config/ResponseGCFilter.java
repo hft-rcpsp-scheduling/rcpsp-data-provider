a2d810ee49684ba6c01b3e8c075f8d87dc47c1b8
felixsteinke <steinke.felix@yahoo.de>
2022-11-08 23:18:43 +0100

    update postman collections for changed api

--- src/main/java/com/hft/provider/config/ResponseGCFilter.java
+++ src/main/java/com/hft/provider/config/ResponseGCFilter.java
@@ -13,12 +13,11 @@ import java.util.logging.Logger;
 public class ResponseGCFilter implements Filter {
     private final Logger LOGGER = Logger.getLogger(ResponseGCFilter.class.getName());
 
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         filterChain.doFilter(servletRequest, servletResponse);
         LOGGER.fine("Running explicit Garbage Collector after " + req.getMethod() + " : " + req.getRequestURI());
-        System.gc();
+        System.gc(); // TODO potential for further improvements (Load Testing + Memory Investigation in the Debugger and Profiler)
     }
 }
