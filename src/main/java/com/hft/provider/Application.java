17b849387a5028799c993b57d8808119acb78ed4
felixsteinke <steinke.felix@yahoo.de>
2022-11-10 15:54:04 +0100

    spring boot update 2.5.10 -> 2.7.5

--- src/main/java/com/hft/provider/Application.java
+++ src/main/java/com/hft/provider/Application.java
@@ -3,11 +3,9 @@ package com.hft.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @SpringBootConfiguration
-@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
